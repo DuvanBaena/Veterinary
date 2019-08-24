3235bf78b67b7c0af4f87af98231c10f66ae311f
duvan baena <djbpuerta@hotmail.com>
2019-08-24 18:06:00 -0500

    Fix create in Owner

--- Veterinary.Web/Controllers/OwnersController.cs
+++ Veterinary.Web/Controllers/OwnersController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Veterinary.Web.Data.Entities;
+using Veterinary.Web.Helpers;
+using Veterinary.Web.Models;
 using Veterinary.Web.Models.Data;
 
 namespace Veterinary.Web.Controllers
@@ -12,10 +16,12 @@ namespace Veterinary.Web.Controllers
     public class OwnersController : Controller
     {
         private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
 
-        public OwnersController(DataContext context)
+        public OwnersController(DataContext context, IUserHelper userHelper)
         {
             _context = context;
+            _userHelper = userHelper;
         }
 
         // GET: Owners
@@ -48,27 +54,65 @@ namespace Veterinary.Web.Controllers
 
             return View(owner);
         }
-
-        // GET: Owners/Create
+        
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Owners/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id")] Owner owner)
+        public async Task<IActionResult> Create(AddUserViewModel model)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(owner);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                var user = new User
+                {
+
+                    Address = model.Address,
+                    Document = model.Document,
+                    Email = model.Username,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    PhoneNumber = model.PhoneNumber,
+                    UserName = model.Username,
+
+                };
+
+                var response = await _userHelper.AddUserAsync(user, model.Password);
+
+                if (response.Succeeded)
+                {
+                    var userInDb = await _userHelper.GetUserByEmailAsync(model.Username);
+                    await _userHelper.AddUserToRoleAsync(userInDb, "Customer");
+
+                    var owner = new Owner
+                    {
+                        Agendas = new List<Agenda>(),
+                        Pets = new List<Pet>(),
+                        User = userInDb,
+                    };
+
+                    _context.Owners.Add(owner);
+
+                    try
+                    {
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction(nameof(Index));
+                    }
+                    catch (Exception ex)
+                    {
+
+                        ModelState.AddModelError(string.Empty, ex.ToString());
+                        return View(model);
+                    }
+
+                }
+
+                ModelState.AddModelError(string.Empty, response.Errors.FirstOrDefault().Description);
             }
-            return View(owner);
+            return View(model);
         }
 
         // GET: Owners/Edit/5
