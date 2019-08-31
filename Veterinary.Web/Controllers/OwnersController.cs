6fea15b1aa8ba002dc81bdbcd40be294bc00e345
duvan baena <djbpuerta@hotmail.com>
2019-08-31 15:54:43 -0500

    Delete History and Pet

--- Veterinary.Web/Controllers/OwnersController.cs
+++ Veterinary.Web/Controllers/OwnersController.cs
@@ -435,5 +435,70 @@ namespace Veterinary.Web.Controllers
             return View(model);
         }
 
+        public async Task<IActionResult> DeleteHistory(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var history = await _dataContext.Histories
+                .Include(h => h.Pet)
+                .FirstOrDefaultAsync(h => h.Id == id.Value);
+            if (history == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _dataContext.Histories.Remove(history);
+                await _dataContext.SaveChangesAsync();
+                return RedirectToAction($"{nameof(DetailsPet)}/{history.Pet.Id}");
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError(string.Empty, ex.ToString());
+                return RedirectToAction($"{nameof(DetailsPet)}/{history.Pet.Id}");
+            }
+        }
+
+        public async Task<IActionResult> DeletePet(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pet = await _dataContext.Pets
+                .Include(p => p.Owner)
+                .Include(p => p.Histories)
+                .FirstOrDefaultAsync(p => p.Id == id.Value);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+
+            if (pet.Histories.Count > 0)
+            {
+
+                ModelState.AddModelError(string.Empty, "The pet can´t be deleted because it has related records");
+                return RedirectToAction($"{nameof(Details)}/{pet.Owner.Id}");
+            }
+
+            try
+            {
+                _dataContext.Pets.Remove(pet);
+                await _dataContext.SaveChangesAsync();
+                return RedirectToAction($"{nameof(Details)}/{pet.Owner.Id}");
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError(string.Empty, ex.ToString());
+                return RedirectToAction($"{nameof(Details)}/{pet.Owner.Id}");
+            }
+        }
     }
 }
