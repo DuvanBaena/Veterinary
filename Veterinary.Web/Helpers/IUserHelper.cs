fb2fbf66637f2006c9a7fc83f2ec847dfadb8f78
duvan baena <djbpuerta@hotmail.com>
2019-08-31 21:06:23 -0500

    Crud Owner

--- Veterinary.Web/Helpers/IUserHelper.cs
+++ Veterinary.Web/Helpers/IUserHelper.cs
@@ -21,6 +21,7 @@ namespace Veterinary.Web.Helpers
 
         Task LogoutAsync();
         Task<bool> DeleteUserAsync(string email);
+
         Task<IdentityResult> UpdateUserAsync(User user);
 
     }
