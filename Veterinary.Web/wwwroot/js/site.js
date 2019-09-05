7ad8cb8d3ed89cc170743700269ed4bc42159e65
duvan baena <djbpuerta@hotmail.com>
2019-09-04 23:18:51 -0500

    modal window design

--- Veterinary.Web/wwwroot/js/site.js
+++ Veterinary.Web/wwwroot/js/site.js
@@ -1,4 +1,18 @@
 ﻿
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
+
+
+
 function deleteItem(Id) {
     var item_to_delete = Id;
     $('#deleteItem').click((e) => {
